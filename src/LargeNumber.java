fe4ffe88df67fbaa314cfb8fa509ab43bc552baf
Ivan Barantskyi <93523707+kreker1207@users.noreply.github.com>
2023-10-18 01:14:51 +0300

    shift left and right but with bugs

--- src/LargeNumber.java
+++ src/LargeNumber.java
@@ -1,3 +1,6 @@
+import java.util.Arrays;
+import java.util.function.IntBinaryOperator;
+
 public class LargeNumber {
 
     private int[] largeValue;
@@ -27,62 +30,103 @@ public class LargeNumber {
     }
 
     public String XOR(LargeNumber numberA, LargeNumber numberB){
-       if(!isValid(numberA) || !isValid(numberB)){
-           throw new IllegalArgumentException("Your number is not valid");
-       }
-       int[] numberArrayA = numberA.getHexInBinary();
-       int[] numberArrayB = numberB.getHexInBinary();
-       int[] result = xorArrays(numberArrayA,numberArrayB);
-       return binaryArrayToHex(result);
+        return performBitwiseOperation(numberA,numberB,(a,b)->a ^ b);
     }
-    ////// Works correctly only for hexes with the same length
-    private int[] xorArrays(int[] arrayA, int[] arrayB) {
-        int length = Math.max(arrayA.length,arrayB.length);
-        int []result = new int[length];
-        for(int i = 0; i< length;i++){
-            int a = (i< arrayA.length)?arrayA[i]:0;
-            int b = (i< arrayB.length)? arrayB[i]:0;
-            result[i] = a ^ b;
-        }
-        return result;
-    }
-
-
     public String OR(LargeNumber numberA, LargeNumber numberB){
-        if(!isValid(numberA) || !isValid(numberB)){
-            throw new IllegalArgumentException("Your number is not valid");
-        }
-        int[] numberArrayA = numberA.getHexInBinary();
-        int[] numberArrayB = numberB.getHexInBinary();
-        int[] result = orArrays(numberArrayA,numberArrayB);
-        return binaryArrayToHex(result);
-    }
-    private int[] orArrays(int[] arrayA, int[] arrayB) {
-        int length = Math.max(arrayA.length,arrayB.length);
-        int []result = new int[length];
-        for(int i = 0; i< length;i++){
-            int bitA = (i < arrayA.length) ? arrayB[i] : 0;
-            int bitB = (i < arrayA.length) ? arrayB[i] : 0;
-            result[i] = bitA | bitB;
-        }
-        return result;
+        return performBitwiseOperation(numberA, numberB, (a, b) -> a | b);
     }
 
     public String AND(LargeNumber numberA, LargeNumber numberB){
-        return "";
+        return performBitwiseOperation(numberA,numberB,(a,b)-> a & b);
     }
-    public String INV(LargeNumber numberA, LargeNumber numberB){
-        return "";
+    public String INV(LargeNumber numberA){
+        if(!isValid(numberA)){
+            throw new IllegalArgumentException("Your number is not valid");
+        }
+        int[] numberBinary = numberA.getHexInBinary();
+        int[] result = new int[numberBinary.length];
+        for(int i= 0; i< result.length;i++){
+            result[i] = (numberBinary[i]==0)?1:0;
+        }
+        return binaryArrayToHex(result);
     }
     public String shiftR(LargeNumber numberA, int n){
-        return "";
+        if(!isValid(numberA)){
+            throw new IllegalArgumentException("Your number is not valid");
+        }
+        if(n<0){throw new IllegalArgumentException("You can not shift to negative number of bits");}
+
+        int[] shiftNumber = numberA.getHexInBinary();
+        int zeroesBeforeOne = 0;
+        for(;zeroesBeforeOne < shiftNumber.length; zeroesBeforeOne++){
+            if(shiftNumber[zeroesBeforeOne] > 0) {
+                break;
+            }
+        }
+        if(shiftNumber.length - n - zeroesBeforeOne <= 0){
+            return "0";
+        }
+        int[] result = new int[shiftNumber.length -n - zeroesBeforeOne];
+        System.arraycopy(shiftNumber,zeroesBeforeOne,result,0,result.length);
+        System.out.println(Arrays.toString(result));
+        return binaryArrayToHex(result);
     }
     public String shiftL(LargeNumber numberA,int n){
-        return "";
+        if(!isValid(numberA)){
+            throw new IllegalArgumentException("Your number is not valid");
+        }
+        if(n<0){throw new IllegalArgumentException("You can not shift to negative number of bits");}
+
+        int[] shiftNumber = numberA.getHexInBinary();
+        System.out.println(Arrays.toString(shiftNumber));
+        int zeroesBeforeOne = 0;
+        for(;zeroesBeforeOne < shiftNumber.length; zeroesBeforeOne++){
+            if(shiftNumber[zeroesBeforeOne] > 0) {
+                break;
+            }
+        }
+        int[] trimArray = new int[shiftNumber.length-zeroesBeforeOne];
+        System.arraycopy(shiftNumber,zeroesBeforeOne,trimArray,0,shiftNumber.length-zeroesBeforeOne);
+        System.out.println(Arrays.toString(trimArray));
+        int newLength = trimArray.length + n;
+        if(newLength <= 0){
+            return "0";
+        }
+        int[] result = new int[newLength];
+        for(int i = newLength-n;i<newLength;i++){
+            Arrays.fill(result,i,newLength,0);
+        }
+        System.arraycopy(trimArray,0,result,0,result.length-1);
+        System.out.println(Arrays.toString(result));
+        return binaryArrayToHex(result);
     }
     public int[] getLargeValue(){
         return this.largeValue;
     }
+
+    private String performBitwiseOperation(LargeNumber numberA, LargeNumber numberB, IntBinaryOperator operation) {
+        if (!isValid(numberA) || !isValid(numberB)) {
+            throw new IllegalArgumentException("Your number is not valid");
+        }
+
+        int[] numberArrayA = numberA.getHexInBinary();
+        int[] numberArrayB = numberB.getHexInBinary();
+        int[] result = applyBinaryOperation(numberArrayA, numberArrayB, operation);
+        return binaryArrayToHex(result);
+    }
+    private int[] applyBinaryOperation(int[] arrayA, int[] arrayB, IntBinaryOperator operation) {
+        int length = Math.max(arrayA.length, arrayB.length);
+        int[] result = new int[length];
+
+        for (int i = 0; i < length; i++) {
+            int a = (i < arrayA.length) ? arrayA[i] : 0;
+            int b = (i < arrayB.length) ? arrayB[i] : 0;
+            result[i] = operation.applyAsInt(a, b);
+        }
+
+        return result;
+    }
+
     private int[] hexStringToIntArray(String hexString){
         hexString = hexString.replaceAll("^0x|0X","");
         if(!isHex(hexString)){
@@ -123,7 +167,7 @@ public class LargeNumber {
         StringBuilder hexNumber = new StringBuilder();
         for (int i = 0; i < binaryArray.length; i += 4) {
             int decimalValue = 0;
-            for (int j = 0; j < 4; j++) {
+            for (int j = 0; j < 4 && i+j<binaryArray.length; j++) {
                 decimalValue = decimalValue * 2 + binaryArray[i + j];
             }
             hexNumber.append(Integer.toHexString(decimalValue));
--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,28 @@
 public class Main {
     public static void main(String[] args) {
-        String hex1 = "24dca";
-        String hex2 = "512acdc";
+        String hex1 = "13";
+        String hex2 = "2033E";
+
         LargeNumber numberA = new LargeNumber();
         numberA.setHex(hex1);
         LargeNumber numberB = new LargeNumber();
         numberB.setHex(hex2);
         String xorResult = new LargeNumber().XOR(numberA,numberB);
         String orResult = new LargeNumber().OR(numberA,numberB);
+        String andResult = new LargeNumber().AND(numberA,numberB);
         System.out.println(xorResult);
+        System.out.println("======================");
         System.out.println(orResult);
+        System.out.println("======================");
+        System.out.println(andResult);
+        System.out.println("======================");
+        System.out.println(new LargeNumber().INV(numberA));
+        System.out.println("======================");
+        System.out.println(new LargeNumber().shiftR(numberA,3));
+        System.out.println("======================");
+        System.out.println(new LargeNumber().shiftL(numberA,1));
+        System.out.println("======================");
+
 
         Tests tests = new Tests();
     }
